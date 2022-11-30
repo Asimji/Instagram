5809fdff16a7d5fbd622978cce55a5a5323acabd
Asimji <mohdasim.srk@gmail.com>
2022-11-30 12:54:44 +0530

    login Completed

--- login.js
+++ login.js
@@ -1,2 +1,13 @@
 let reg_email="asim@gmail.com";
-let reg_pass="123@";
+let reg_pass="12345@";
+
+let enter_email="asim@gmail.com";
+let enter_pass="12345@";
+
+if(enter_email==reg_email && enter_pass==reg_pass){
+
+console.log("login successful");
+}
+else{
+console.log("wrong creditential");
+}
